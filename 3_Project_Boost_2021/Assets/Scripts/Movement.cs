5e5ba02b19e2f2d2dba8965938bc1522d2ef511e
Harishkumarharry <54526806+Harishkumarharry@users.noreply.github.com>
2021-05-25 20:48:36 +0530

    2nd commit
    Wrote Movement script. Changed Prefab of Rocket. Added Audio Source for the Rocket. Added Obstacles.

--- 3_Project_Boost_2021/Assets/Scripts/Movement.cs
+++ 3_Project_Boost_2021/Assets/Scripts/Movement.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody rb;
+    AudioSource audioSource;
+
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
     }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            RocketRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            RocketRotation(-rotationThrust);
+        }
+    }
+
+    void RocketRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // Frezzing rotation so we can manually rotate our Rocket.
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // UnFrezzing rotation so we can manually rotate our Rocket.
+    }
+
 }
