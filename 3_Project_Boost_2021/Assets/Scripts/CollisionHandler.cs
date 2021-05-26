c8e16bec72594cc654307ffd4136a5f55ac0ee33
Harishkumarharry <54526806+Harishkumarharry@users.noreply.github.com>
2021-05-26 20:49:30 +0530

    3rd commit
    Making Custom Rocket. Adding Multiple Audio Source. Cycling through different levels/scenes in the game.

--- 3_Project_Boost_2021/Assets/Scripts/CollisionHandler.cs
+++ 3_Project_Boost_2021/Assets/Scripts/CollisionHandler.cs
@@ -1,18 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] AudioClip crashSound;
+    [SerializeField] AudioClip successSound;
+
+    AudioSource audioSource;
+
+    bool isTransitioning = false;
+
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision collision)
     {
-        
+        if (isTransitioning) { return; }
+        else
+        {
+            switch (collision.gameObject.tag)
+            {
+                case "Friendly":
+                    Debug.Log("Friendly object hit");
+                    break;
+                case "Finish":
+                    StartSuccessSequence();
+                    break;
+                default:
+                    StartCrashSequence();
+                    break;
+            }
+        }
     }
+
+    void StartCrashSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(crashSound);
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    void StartSuccessSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(successSound);
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", loadDelay);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    void ReloadLevel()
+    {
+        int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(CurrentSceneIndex);
+    }
+      
 }
